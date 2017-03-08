910f5f618addc372eaf9a4009bd24b310fc50593
vindavadilla <vindavadilla080@gmail.com>
2017-03-08 10:16:10 +0800

    Posttest2

---
+++ src/POSTTEST2/VISUAL_C2_084.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package POSTTEST2;
+
+/**
+ *
+ * @author vinda
+ */
+public class VISUAL_C2_084 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
